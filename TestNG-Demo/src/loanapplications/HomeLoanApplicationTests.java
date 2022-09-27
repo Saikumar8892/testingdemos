3069d4f1da1491f56b1a52c8b90d37f9cd109e5c
github_Saikumar8892 <sairoyalsulli@gmail.com>
2022-09-27 21:40:29 +0530

    changes

---
+++ TestNG-Demo/src/loanapplications/HomeLoanApplicationTests.java
@@ -0,0 +1,23 @@
+package loanapplications;
+
+import org.testng.annotations.Test;
+
+public class HomeLoanApplicationTests {
+
+	@Test
+	public void CarLoanLoginWeb() {
+		System.out.println("Home loan application - web login test");
+	}
+	
+	@Test
+	public void CarLoanLoginMobile() {
+		System.out.println("Home loan application - mobile login test");
+	}
+	
+	@Test
+	public void CarLoanLoginAPI() {
+		System.out.println("Home loan application - API login test");
+	}
+	
+}
+
