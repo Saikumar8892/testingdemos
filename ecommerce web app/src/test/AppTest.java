864d40fb4bc40de404c12dda468c5ec1c8d5feb9
github_Saikumar8892 <sairoyalsulli@gmail.com>
2022-10-11 21:44:18 +0530

    changes

--- ecommerce web app/src/test/AppTest.java
+++ ecommerce web app/src/test/AppTest.java
@@ -6,11 +6,12 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
 
 public class AppTest {
 	WebDriver driver;
-
+	
 	@Test(groups = "Chrome")
 	public void LaunchChrome() {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\saikumar\\Downloads\\chromedriver_win32\\chromedriver.exe");
@@ -22,7 +23,7 @@ public class AppTest {
 	}
 
 	@Test(groups = "Chrome", dependsOnMethods = "LaunchChrome")
-	public void f() {
+	public void Chrome() {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.flipkart.com/");
@@ -66,5 +67,63 @@ public class AppTest {
 			e.printStackTrace();
 		}
 	}
+
+	@Test(groups = "edge")
+	public void LaunchEdge() {
+		System.setProperty("webdriver.edge.driver","C:\\Users\\saikumar\\Downloads\\edgedriver_win64\\msedgedriver.exe");
+
+		try {
+			Thread.sleep(2000);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 	
+	@Test(groups="Edge", dependsOnMethods="LaunchEdge")
+	public void Edge() {
+		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.flipkart.com/");
+		
+		//		To find the load time and print on console
+
+		Long loadtime = (Long) ((JavascriptExecutor) driver)
+				.executeScript("return performance.timing.loadEventEnd - performance.timing.navigationStart;");
+		System.out.println("Load time of the website is :- " + loadtime/1000+" Seconds");
+
+//		To search the given text in search bar
+
+		driver.findElement(By.className("_3704LK")).sendKeys("iphone 13");
+		driver.findElement(By.className("L0Z3Pu")).click();
+
+		WebElement i = driver.findElement(By.xpath("//img[@class='_396cs4 _3exPp9']"));
+		Boolean p = (Boolean) ((JavascriptExecutor) driver).executeScript("return arguments[0].complete "
+				+ "&& typeof arguments[0].naturalWidth != \"undefined\" " + "&& arguments[0].naturalWidth > 0", i);
+
+		if (p) {
+			System.out.println("Image is loaded");
+		} else {
+			System.out.println("Image not loaded");
+		}
+
+		try {
+			long lastHeight = (long) ((JavascriptExecutor) driver).executeScript("return document.body.scrollHeight");
+
+			while (true) {
+				((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight);");
+				Thread.sleep(2000);
+
+				long newHeight = (long) ((JavascriptExecutor) driver)
+						.executeScript("return document.body.scrollHeight");
+				if (newHeight == lastHeight) {
+					break;
+				}
+				lastHeight = newHeight;
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
